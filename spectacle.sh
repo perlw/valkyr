222e83681b995b13c6239b045a285437ccd9654d
Per Lönn Wege <per@perlw.se>
2018-04-16 07:09:00 +0200

    Mispelling <.< >.>

--- spectacle.sh
+++ spectacle.sh
@@ -3,6 +3,6 @@ go get -u golang.org/x/vgo &> goget.log
 $GOPATH/bin/vgo build -o bin/valkyr &> build.log
 
 pkill valkyr
-cp bin/valky $HOME/services/
+cp bin/valkyr $HOME/services/
 cd $HOME/services
 nohup ./valkyr &> valkyr.log &
