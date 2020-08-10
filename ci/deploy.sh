20d5c1aa54b1dd24f4125edcc1aba94efb945ef4
Per Lönn Wege <per@perlw.se>
2020-08-10 11:45:29 +0200

    Incorrect order

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,8 +2,8 @@
 if [ "$(docker network ls -qf \"name=valkyr\")" == ""]; then
   docker network create valkyr;
 fi;
-gunzip -c images/valkyr.tar.gz | docker load
 dockerid=$(docker ps -qf "ancestor=valkyr")
+gunzip -c images/valkyr.tar.gz | docker load
 docker stop $dockerid
 docker run --rm -d -p 8001:80 -p 8444:443 valkyr:latest
 docker container prune -f
