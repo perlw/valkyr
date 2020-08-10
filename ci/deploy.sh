aea61f8361932cb6ce40ab74628b63aef36ddf25
Per Lönn Wege <per@perlw.se>
2020-08-10 11:44:57 +0200

    Finalize deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-if [ "$(docker network ls -qf "name=valkyr")" == ""]; then
+if [ "$(docker network ls -qf \"name=valkyr\")" == ""]; then
   docker network create valkyr;
 fi;
-echo "name $APP_NAME"
-#dockerid=$(ssh -o StrictHostKeyChecking=no -i ./key \
-#$SSH_HOST \ "docker ps -qf \"ancestor=$APP_NAME\"")
-#ssh -o StrictHostKeyChecking=no -i ./key $SSH_HOST \
-#'gunzip | docker load' < image.tar.gz
-#docker stop $dockerid
-#docker run --rm -d -p 8001:80 -p 8444:443 valkyr:latest
-#docker container prune -f
-#docker image prune -af
+gunzip -c images/valkyr.tar.gz | docker load
+dockerid=$(docker ps -qf "ancestor=valkyr")
+docker stop $dockerid
+docker run --rm -d -p 8001:80 -p 8444:443 valkyr:latest
+docker container prune -f
+docker image prune -af
