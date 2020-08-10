81b9106a620a1569297b57bbf9881cb5e0e19795
Per Lönn Wege <per@perlw.se>
2020-08-10 17:12:28 +0200

    Remove custom network layer

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-if [ "$(docker network ls -qf \"name=valkyr\")" == ""]; then
-  docker network create valkyr;
-fi;
 dockerid=$(docker ps -qf "ancestor=valkyr")
 gunzip -c images/valkyr.tar.gz | docker load
-if [ "$dockerid" != ""]; then
+if [ "$dockerid" != "" ]; then
   docker stop $dockerid
 fi;
-docker run --restart unless-stopped -d \
-  --net=host --net=valkyr \
-  valkyr:latest
+docker run --restart unless-stopped -d --net=host valkyr:latest
 docker container prune -f
 docker image prune -af
