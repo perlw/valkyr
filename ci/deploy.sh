35ea79569dccfbb3d02d9c34e45466ed8a45f8c5
Per Lönn Wege <per@perlw.se>
2020-08-10 11:58:32 +0200

    Finalize deployscript

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,7 +4,11 @@ if [ "$(docker network ls -qf \"name=valkyr\")" == ""]; then
 fi;
 dockerid=$(docker ps -qf "ancestor=valkyr")
 gunzip -c images/valkyr.tar.gz | docker load
-docker stop $dockerid
-docker run --rm -d -p 8003:80 -p 8444:443 valkyr:latest
+if [ "$dockerid" != ""]; then
+  docker stop $dockerid
+fi;
+docker run --restart unless-stopped -d \
+	-p 8000:80 -p 8443:443 --net=valkyr \
+	valkyr:latest
 docker container prune -f
 docker image prune -af
